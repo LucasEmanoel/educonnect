e58f9fb682d9e0dcf4e3ea4432cb339336b42bfd
Lucas Emanoel <lucas2014.barros@gmail.com>
2023-03-22 07:26:24 -0300

    Feat: initial commit packages

---
+++ src/test/java/br/com/educonnect/EduconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.educonnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

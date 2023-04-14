04bcf9f5889601745d6d85d715d675bb42c15281
TayaneCibely <tayanerodrigues2001@gmail.com>
2023-04-14 18:23:34 -0300

    hoje

---
+++ src/main/java/br/com/educonnect/comunicacao/ControllerEmenta.java
@@ -0,0 +1,65 @@
+package br.com.educonnect.comunicacao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.educonnect.negocio.basica.Disciplina;
+import br.com.educonnect.negocio.basica.Ementa;
+import br.com.educonnect.negocio.cadastro.CadastroDisciplina;
+import br.com.educonnect.negocio.cadastro.CadastroEmenta;
+import br.com.educonnect.negocio.cadastro.DisciplinaNaoExisteException;
+import br.com.educonnect.negocio.cadastro.EmentaNaoExisteException;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+public class ControllerEmenta {
+
+    @Autowired
+    private CadastroEmenta cadastroEmenta;
+
+    @Autowired
+    private CadastroDisciplina cadastroDisciplina;
+
+    @PostMapping(value = "/ementa/disciplina/{disciplinaId}")
+    public ResponseEntity<Ementa> salvarEmenta(@RequestBody Ementa ementa, @PathVariable long disciplinaId) {
+        try {
+            Disciplina disciplina = cadastroDisciplina.procurarDisciplinaId(disciplinaId);
+            ementa.setDisciplina(disciplina);
+            return ResponseEntity.ok(cadastroEmenta.salvarEmenta(ementa));
+        } catch (DisciplinaNaoExisteException e) {
+            e.printStackTrace();
+            return new ResponseEntity<Ementa>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/ementa/{id}")
+    public ResponseEntity<Object> deleteEmenta(@PathVariable("id") long id) {
+        cadastroEmenta.deletarEmentaId(id);
+		return ResponseEntity.ok("Deletado com sucesso");
+    }
+
+    @GetMapping(value = "/ementa/{id}")
+    public ResponseEntity<Ementa> procurarEmenta(@PathVariable("id") long id) {
+        try {
+            return ResponseEntity.ok(cadastroEmenta.procurarEmentaId(id));
+        } catch (EmentaNaoExisteException e) {
+            e.printStackTrace();
+            return new ResponseEntity<Ementa>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(value = "/ementas")
+    public ResponseEntity<List<Ementa>> listEmentas() {
+        return ResponseEntity.ok(cadastroEmenta.listarEmentas());
+    }
+}
--- src/main/java/br/com/educonnect/negocio/cadastro/ICadastroDisciplina.java
+++ src/main/java/br/com/educonnect/negocio/cadastro/ICadastroDisciplina.java
@@ -3,7 +3,6 @@ package br.com.educonnect.negocio.cadastro;
 import java.util.List;
 
 import br.com.educonnect.negocio.basica.Disciplina;
-import br.com.educonnect.negocio.basica.Matricula;
 
 public interface ICadastroDisciplina {
 	Disciplina procurarDisciplinaId(long id) throws DisciplinaNaoExisteException;

687119633bc567853cbcb7d5daf82642ee3937ac
TayaneCibely <87872064+TayaneCibely@users.noreply.github.com>
2023-04-16 15:48:17 -0300

    universidade m

--- src/main/java/br/com/educonnect/comunicacao/UniversidadeController.java
+++ src/main/java/br/com/educonnect/comunicacao/UniversidadeController.java
@@ -1,50 +1,66 @@
 package br.com.educonnect.comunicacao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.educonnect.negocio.basica.Universidade;
-import br.com.educonnect.negocio.cadastro.ICadastroUniversidade;
 import br.com.educonnect.negocio.cadastro.UniversidadeJaExisteException;
+import br.com.educonnect.negocio.cadastro.UniversidadeNaoExisteException;
+import br.com.educonnect.negocio.fachada.Fachada;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins="http://localhost:3000/")
 @RestController
 public class UniversidadeController {
+	@Autowired
+	private Fachada fachada;
 
-    @Autowired
-    private ICadastroUniversidade cadastroUniversidade;
+	@PostMapping("/universidade")
+	public ResponseEntity<Universidade> salvarUniversidade(@RequestBody Universidade universidade) throws UniversidadeJaExisteException{
+		return ResponseEntity.ok(fachada.salvarUniversidade(universidade));
+	}
+	
+	@DeleteMapping(value = "/universidade/{id}")
+	public ResponseEntity<Object> deleteUniversidade0(@PathVariable("id") long id){
+		this.fachada.deletarUniversidade(id);
+		return ResponseEntity.ok("Deletado com sucesso");
+	}
+	
+	@GetMapping(value = "/universidade/{id}")
+	public ResponseEntity<Universidade> procurarUniversidade(@PathVariable("id") long id){
+		try {
+			return ResponseEntity.ok(fachada.encontrarUniversidadeId(id));
+		} catch (UniversidadeNaoExisteException e) {
+			e.printStackTrace();
+			return new ResponseEntity<Universidade> (HttpStatus.BAD_REQUEST);
+		}
+	}
 
-    @PostMapping("/universidade")
-    public ResponseEntity<Universidade> salvarUniversidade(@RequestBody Universidade universidade) {
-        try {
-            Universidade uni = this.cadastroUniversidade.cadastrarUniversidade(universidade);
-            return ResponseEntity.ok(uni);
-        } catch (UniversidadeJaExisteException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+	@GetMapping("/universidades")
+	public ResponseEntity<List<Universidade>> listUniversidade(){
+		return ResponseEntity.ok(fachada.listarUniversidade());
+	}
+	
+	@PatchMapping(value = "/universidade/{id}")
+	public ResponseEntity<Universidade> atualizarUniversidade(@RequestBody Universidade uni, @PathVariable long id){
+		try {
+			return ResponseEntity.ok(fachada.atualizarUniversidade(uni, id));
+		} catch(UniversidadeNaoExisteException e) {
+			e.printStackTrace();
+			return new ResponseEntity<Universidade> (HttpStatus.BAD_REQUEST);	
+		}
 
-    @GetMapping("/universidade")
-    public ResponseEntity<Universidade> encontrarUniversidade(Long id) {
-        Universidade uni = this.cadastroUniversidade.getUniversidade(id);
-        if (uni != null) {
-            return ResponseEntity.ok(uni);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
+	}
 
-    @DeleteMapping("/universidade/{id}")
-    public ResponseEntity<Void> removerUniversidade(@PathVariable Long id) {
-        this.cadastroUniversidade.removerUniversidade(id);
-		return ResponseEntity.ok().build();
-    }
 }
+
--- src/main/java/br/com/educonnect/negocio/cadastro/ICadastroUniversidade.java
+++ src/main/java/br/com/educonnect/negocio/cadastro/ICadastroUniversidade.java
@@ -1,17 +1,19 @@
 package br.com.educonnect.negocio.cadastro;
 
+import java.util.List;
+
 import br.com.educonnect.negocio.basica.Universidade;
 
 public interface ICadastroUniversidade {
 	
-    Universidade cadastrarUniversidade(Universidade universidade) throws UniversidadeJaExisteException;
-    
-    Universidade getUniversidade(Long id);
-    
-    void setUniversidade(Universidade universidade);
-
-	void removerUniversidade(Long id);
-
+	Universidade encontrarUniversidadeId(long id) throws UniversidadeNaoExisteException;
 	
+    Universidade salvarUniversidade(Universidade universidade);
+    
+    List<Universidade> listarUniversidade();
+    
+    void deletarUniversidadeId(long id);
+    
+    Universidade atualizarUniversidade(Universidade universidade, long id) throws UniversidadeNaoExisteException;
     
 }
